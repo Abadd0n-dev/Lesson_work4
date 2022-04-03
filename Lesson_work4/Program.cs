58cbc564ed66acb8a45e0327e200a4608d8816b5
rkutaraev@list.ru <rkutaraev@list.ru>
2022-04-03 09:07:04 +0300

    Создана программа ,принимающая на входную строку,набор чисел через пробел и возвращающую сумму всех чисел в строке.

--- Lesson_work4/Program.cs
+++ Lesson_work4/Program.cs
@@ -10,7 +10,22 @@ namespace Lesson_work4
     {
         static void Main(string[] args)
         {
-
+            string output = StringOutput();
+            int spacereturns = linebreak(output);
+            Console.WriteLine(spacereturns);
+            Console.ReadLine();
+        }
+        static string StringOutput()
+        {
+            Console.Write("Введите последовательность чисел через пробел: ");
+            return Console.ReadLine();
+        }
+        static int linebreak(string output)
+        {
+            var words = output.Split();
+            int part = default;
+            for (int i = 0; i < words.Length; part += int.Parse(words[i++])) ;
+            return part;
         }
     }
 }
